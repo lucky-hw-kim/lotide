e606d64325151aa243149ffbb490568501b5f13b
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-07 22:02:24 +0000

    implement assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPRELEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp')
+assertEqual(1, 1)
+assertEqual('string', 'string')
+assertEqual('string', 'non-string')
+assertEqual(3, 3)
+assertEqual(6, 3)
