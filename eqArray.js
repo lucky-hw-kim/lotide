78d9d0716ec1ff550fb82e08994994e0f8822bb5
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-08 21:01:08 +0000

    Add assertArraysEqual.js

--- eqArray.js
+++ eqArray.js
@@ -1,21 +1,16 @@
 // FUNCTION IMPRELEMENTATION
+
 function eqArrays (arr1, arr2) {
-  let arrayLength = 0
-  if (arr1.length === arr2.length) {
-    arrayLength = arr1.length
-  } else {
+  if (arr1.length !== arr2.length) {
     return false
   }
 
-  for (let i = 0; i < arrayLength; i++) {
-    if (arr1[i] === arr2[i]) {
-      arr1.push(arr2[i])
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
     }
-  } if (arr1.length === arrayLength * 2) {
-    return true
-  } else {
-    return false
   }
+  return true
 }
 
 const assertEqual = function (actual, expected) {
@@ -26,11 +21,11 @@ const assertEqual = function (actual, expected) {
   }
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => treu
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// eqArrays([1, 2, 3], [1, 2, 3]) // => treu
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(['1', '2', '3'], ['1', '2', '3']) // => true
-eqArrays(['1', '2', '3'], ['1', '2', 3]) // => false
+// eqArrays(['1', '2', '3'], ['1', '2', '3']) // => true
+// eqArrays(['1', '2', '3'], ['1', '2', 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
 assertEqual(eqArrays([1, 3, 2], [1, 2, 3]), false) // => should PASS
