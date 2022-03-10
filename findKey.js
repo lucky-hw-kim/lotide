39f03b4f9d28a1217c07fc911eb94d6ec63debe7
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-10 16:37:06 +0000

    add findkeys.js and fixed takeUntil.js

---
+++ findKey.js
@@ -0,0 +1,40 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const findKey = function (obj, callback) {
+  const values = Object.values(obj)
+  const keys = Object.keys(obj)  
+  let result;
+  console.log(keys);
+
+  for(let i = 0; i < values.length; i++){
+    if(callback(values[i])) {
+      result = keys[i];
+      break;
+    }
+  }
+  return result
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual (findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma')
--- takeUntil.js
+++ takeUntil.js
@@ -26,6 +26,30 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+function eqArrays (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function assertArraysEqual (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+}
+
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ])
+assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ])
+
 //Expected Output
 /*
 [ 1, 2, 5, 7, 2 ]
