18ad788479c3d28c247d20a708c2929ba2595a47
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-11 22:17:19 +0000

    Add eqObjects-recursive

---
+++ eqObjects-recursive.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unreachable-loop */
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+function eqArrays (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const eqObjects = function (object1, object2) {
+  const obj1Keys = Object.keys(object1)
+  const obj2Keys = Object.keys(object2)
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false
+  } else {
+    for (const key in object1) {
+      if (typeof object1[key] !== typeof object2[key]) {
+        return false
+      }
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false
+        }
+      } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false
+        }
+      } else if ((object1[key] !== object2[key])) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+// const ab = { a: '1', b: '2' }
+// const ba = { b: '2', a: '1' }
+// assertEqual(eqObjects(ab, ba), true) // => true
+
+// const abc = { a: '1', b: '2', c: '3' }
+// assertEqual(eqObjects(ab, abc), false) // => false
+
+// const cd = { c: '1', d: ['2', 3] }
+// const dc = { d: ['2', 3], c: '1' }
+// assertEqual(eqObjects(cd, dc), true) // => true
+
+// const cd2 = { c: '1', d: ['2', 3, 4] }
+// assertEqual(eqObjects(cd, cd2), false) // => false
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)// => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2, d: [3] }, { a: 1, b: 2 }), false)
